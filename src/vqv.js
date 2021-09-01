a99a34f4b317f146d91223448fb74a5bf54e169e
Rodrigo Lima <limarodrigoo@outlook.com>
2021-09-01 14:46:54 -0300

    add vqv function

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const str = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  return str;
+};
 
 module.exports = vqv;
